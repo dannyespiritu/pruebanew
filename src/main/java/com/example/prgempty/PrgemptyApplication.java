df92ed05cb94cef6603ff1b05be52c9b99648c8d
dannyespiritu <dannyespiritu@gmail.com>
2025-04-06 21:52:25 -0500

    nada

---
+++ src/main/java/com/example/prgempty/PrgemptyApplication.java
@@ -0,0 +1,13 @@
+package com.example.prgempty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrgemptyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrgemptyApplication.class, args);
+	}
+
+}
